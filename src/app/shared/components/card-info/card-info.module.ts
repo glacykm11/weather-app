91a5949ba7d2f0a7bf0a1eaf9ea37c1189b9896b
Glacy Gomes <glacy.kelly3@gmail.com>
2023-07-15 00:44:09 -0300

    feat: implementando novo cardInfoComponent

---
+++ src/app/shared/components/card-info/card-info.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardInfoComponent } from './card-info.component';
+
+@NgModule({
+  declarations: [
+    CardInfoComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CardInfoComponent
+  ],
+})
+export class CardInfoModule { }
