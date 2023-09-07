0e7832585d74f6b116e230c267e6756e0a12f799
Glacy Gomes <glacy.kelly3@gmail.com>
2023-09-07 09:29:15 -0300

    fix: ajustes nomenclaturas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { WheatherMapService } from '../shared/services/wheather-map/wheather-map.service';
 import { Observable, take } from 'rxjs';
 import { TodayForecast } from '../shared/interfaces/weather-forecast.interface';
-import { WeatherInfo } from '../shared/interfaces/weather-info.interface';
-import {
-  Hour,
-  WeatherApiForecast,
-} from '../shared/interfaces/weather-api-forecast.interface';
+import { Hour } from '../shared/interfaces/weather-api-forecast.interface';
 import { WeatherApiService } from '../shared/services/weather-api/weather-api.service';
+import { WeatherInitialInfo } from '../shared/interfaces/weather-info.interface';
 
 @Component({
   selector: 'app-home',
@@ -20,8 +17,8 @@ export class HomeComponent implements OnInit {
   public description!: any;
   public todayForecasts: TodayForecast[] = [];
   public seeMoreButton: boolean = false;
-  public weatherInfoSlz$!: any;
-  private weatherApiForecastSlz$!: Observable<WeatherApiForecast>;
+  public weatherInfoSlz$!: Observable<WeatherInitialInfo[]>;
+  private weatherApiForecastSlz$!: Observable<Hour[]>;
 
   constructor(
     private wheatherMapService: WheatherMapService,
@@ -30,11 +27,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.getWheatherSlz();
-    // this.getSlzWeatherForecast();
+    this.getSlzWeatherForecast();
   }
 
-  public teste(event: any) {
-    console.log('Capturando clique botão saiba mais');
+  public seeMoreButtonEvent(event: any) {
     this.seeMoreButton = !this.seeMoreButton;
   }
 
@@ -45,27 +41,19 @@ export class HomeComponent implements OnInit {
   private getSlzWeatherForecast(): void {
     this.weatherApiForecastSlz$ = this.weatherApiService.getForecast();
 
-    this.weatherApiForecastSlz$
-      .pipe(take(1))
-      .subscribe((response: WeatherApiForecast) => {
-        const hours = response.forecast.forecastday[0].hour;
-        const todayForecasts = this.generateTodayForecasts(hours);
-
-        this.todayForecasts = this.todayForecasts.concat(...todayForecasts);
-      });
+    this.weatherApiForecastSlz$.pipe(take(1)).subscribe((response: Hour[]) => {
+      const hours = response;
+      this.generateTodayForecasts(hours);
+    });
   }
 
-  private generateTodayForecasts(hours: Hour[]): TodayForecast[] {
-    const todayForecasts = [];
-
+  private generateTodayForecasts(hours: Hour[]): void {
     for (let i = 6; i < 22; i += 3) {
-      todayForecasts.push({
+      this.todayForecasts.push({
         time: hours[i].time.substring(11),
         temperature: hours[i].temp_c,
         icon: hours[i].condition.icon,
       });
     }
-
-    return todayForecasts;
   }
 }
