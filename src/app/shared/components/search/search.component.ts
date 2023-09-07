57d063ddbf53cbd2a669196a84d81ebd4c48a4d7
Glacy Gomes <glacy.kelly3@gmail.com>
2023-09-07 10:58:45 -0300

    feat: adding route to cities by clicking in search component

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  cities = new FormControl('');
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  public navigateToCitiesPage(): void {
+    this.router.navigate(['/cities']);
+  }
 }
--- src/app/shared/components/search/search.module.ts
+++ src/app/shared/components/search/search.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    SearchComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    SearchComponent
-  ],
+  declarations: [SearchComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  exports: [SearchComponent],
 })
-export class SearchModule { }
+export class SearchModule {}
