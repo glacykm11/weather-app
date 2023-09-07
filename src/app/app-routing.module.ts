cd5c39cd4c28b3f529e03852a02c15e3ba67aafd
Glacy Gomes <glacy.kelly3@gmail.com>
2023-09-07 09:17:32 -0300

    fix: ajustes formatação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: "",
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  }
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
